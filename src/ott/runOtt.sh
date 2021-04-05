c7ec067fe69e35ae1202e20760dc3395763c1384
Matthew Hammer <matthew@dfinity.org>
2021-04-05 15:23:25 -0600

    distinguish examples vs overview prose

--- src/ott/runOtt.sh
+++ src/ott/runOtt.sh
@@ -15,6 +15,13 @@ ott \
     -i fumola.ott \
     -tex_filter examples.mng examples.tex
 
+ott \
+    -tex_wrap true \
+    -i fumola.ott \
+    -tex_filter overview.mng overview.tex
+
 pdflatex fumola.tex || echo Expect manual intervention here, sometimes.
 
 pdflatex examples.tex
+
+pdflatex overview.tex
