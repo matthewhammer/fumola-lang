69a641d2c9684459bcc5e32a9b3b4882e440e882
Matthew Hammer <matthew.hammer@gmail.com>
2021-06-24 07:55:05 -0600

    Parser for Fumola AST

---
+++ rust/src/bin/fumola.rs
@@ -0,0 +1,81 @@
+
+use structopt::StructOpt;
+
+use clap::Shell;
+use log::info;
+
+//use candid::Decode;
+use ic_agent::Agent;
+use ic_types::Principal;
+
+
+use std::io;
+use std::time::Duration;
+
+use fumola::error::OurResult;
+
+/// Fumola tools
+#[derive(StructOpt, Debug, Clone)]
+#[structopt(
+    name = "fumola",
+    setting = clap::AppSettings::DeriveDisplayOrder
+)]
+pub struct CliOpt {
+    /// Trace-level logging (most verbose)
+    #[structopt(short = "t", long = "trace-log")]
+    pub log_trace: bool,
+    /// Debug-level logging (medium verbose)
+    #[structopt(short = "d", long = "debug-log")]
+    pub log_debug: bool,
+    /// Coarse logging information (not verbose)
+    #[structopt(short = "L", long = "log")]
+    pub log_info: bool,
+
+    #[structopt(subcommand)]
+    pub command: CliCommand,
+}
+
+#[derive(StructOpt, Debug, Clone)]
+pub enum CliCommand {
+    #[structopt(
+        name = "completions",
+        about = "Generate shell scripts for auto-completions."
+    )]
+    Completions { shell: Shell },
+}
+
+fn init_log(level_filter: log::LevelFilter) {
+    use env_logger::{Builder, WriteStyle};
+    let mut builder = Builder::new();
+    builder
+        .filter(None, level_filter)
+        .write_style(WriteStyle::Always)
+        .init();
+}
+
+#[tokio::main]
+async fn main() -> OurResult<()> {
+    info!("Starting...");
+    let cli_opt = CliOpt::from_args();
+    let cc = {
+        let cli_opt = cli_opt.clone();
+    };
+    info!("Init log...");
+    init_log(
+        match (cli_opt.log_trace, cli_opt.log_debug, cli_opt.log_info) {
+            (true, _, _) => log::LevelFilter::Trace,
+            (_, true, _) => log::LevelFilter::Debug,
+            (_, _, true) => log::LevelFilter::Info,
+            (_, _, _) => log::LevelFilter::Warn,
+        },
+    );
+    info!("Evaluating CLI command: {:?} ...", &cli_opt.command);
+    let () = match cli_opt.command {
+        CliCommand::Completions { shell: s } => {
+            // see also: https://clap.rs/effortless-auto-completion/
+            CliOpt::clap().gen_completions_to("caniput", s, &mut io::stdout());
+            info!("done");
+        }
+    };
+    Ok(())
+}
